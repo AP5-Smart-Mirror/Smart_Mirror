2470e265f396e076478e9c1dc016d77eac25a1c7
Guillaume <guillaume.vanhelst@gmail.com>
2020-09-25 10:41:34 +0200

    BACK: comment code
    - comment on clock and weather files

--- back/weather.js
+++ back/weather.js
@@ -1,35 +1,34 @@
-var fetch = require('node-fetch');
-var apiKey = 'c0735eb48963d2eea782623d14502b53';
-var city = 'Lille';
-var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+var fetch = require('node-fetch');                //load the module node-fetch
+var apiKey = 'c0735eb48963d2eea782623d14502b53';  //personal key to get weather data 
+var city = 'Lille';                               //city generation
+var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;   //url using previous variables
 
 function jsonTreatment(json){
     var newJson = {};
-    newJson['currenttemp'] = json['main']['temp'];
-    newJson['city'] = city;
+    newJson['currenttemp'] = json['main']['temp'];    //recuperation of temperature
+    newJson['city'] = city;                           
     newJson['iconurl'] = 'http://openweathermap.org/img/wn/'+ json['weather'][0]['icon'] +'.png'
     return newJson
 }
 
 function getWeatherProm() {
-    return new Promise((success,failure) => {
-        fetch(url,
+    return new Promise((success,failure) => {       //declaration of promise
+        fetch(url,                                  //url indication
             {
                 headers:{
-                    'Accept':'application/json'
+                    'Accept':'application/json'     //want json
                 }
             })           
-            .then(response => {
+            .then(response => {                     //display the response in json
                 success(response.json()); 
             })
-            .catch(err => console.error(err));
+            .catch(err => console.error(err));      //display the error in console
  
     })
 };
 
 async function getWeather() {
-    return jsonTreatment(await getWeatherProm());
-}
+    return jsonTreatment(await getWeatherProm());   //return value jsonTreatment which use the return value of getWeatherProm
 
 
-exports.getWeather = getWeather;
+exports.getWeather = getWeather;             //export function getWeather
