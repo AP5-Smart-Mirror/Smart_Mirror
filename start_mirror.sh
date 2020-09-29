c931e5c19bbb70151d84e22ea9714d021f5ae9bb
Adrien <legrand.adrien96@gmail.com>
2020-09-29 17:21:20 +0200

    Update the start script
    don't forget to execute it in the autostart file

--- start_mirror.sh
+++ start_mirror.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+# Git pull
+#cd /home/pi/Documents/Smart-Mirror/ && git pull &&
+#cd /var/www/html/ && git pull &&
 
-cd ./Desktop/Smart-Mirror/back/
-npm run start &
+# start the servers
+cd /home/pi/Documents/Smart-Mirror/back/ && npm run start &
+sudo systemctl restart lighttpd &
 
-cd ../front/
-npm run start &
+
+# start chromium in kiosk mode
+xset s noblank
+xset s off
+xset -dpms
+
+unclutter -idle 0.5 -root &
+
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:4200/ &
+
