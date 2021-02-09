ff4f4fdc2cbf42f3c6b855abc5938cae4d916c44
GaspardMathon <gaspard.mathon@decathlon.com>
2021-02-09 09:24:01 +0100

    add website bar

---
+++ front/src/app/website/website-bar/website-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-website-bar',
+  templateUrl: './website-bar.component.html',
+  styleUrls: ['./website-bar.component.css']
+})
+export class WebsiteBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
