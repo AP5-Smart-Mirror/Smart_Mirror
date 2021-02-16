51cc525e602213ba44ae0762caa49ae15cbda826
Rainstorm <legrand.adrien96@gmail.com>
2021-02-16 15:53:31 +0100

    cleaning

--- back/src/Google/routes/googleRouter.js
+++ back/src/Google/routes/googleRouter.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var gCal = require('../components/google_calendar.js');
+var gMail = require('../components/google_mail.js');
 var router = express.Router();
 
 /* GET auth callback. */
 router.get('/calendar', function (req, res, next) {
   res.send(gCal.getCalendar());
+});
+
+router.get('/mails', function (req, res, next) {
+  res.send(gMail.getMail());
 });
 
 module.exports = router;
