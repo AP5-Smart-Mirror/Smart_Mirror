e8ec4b552211281045416c30c687d0ef10e8dd7c
Guillaume <guillaume.vanhelst@gmail.com>
2021-02-17 09:51:51 +0100

    BACK: update login file
    - Change login file

--- back/bdd/src/login/components/login.js
+++ back/bdd/src/login/components/login.js
@@ -1,20 +1,24 @@
 var connection = require('../../Connection/connection').mysql_pool;
 
 async function getLogin(body) {
+  let idProfil = body.idProfil;
   let nom = body.nom;
   let motWEB = body.motWEB;
   connection.getConnection(function (err) {
     if (err) throw err;
     console.log('Connected!');
     var sql =
-      "INSERT INTO profil (nom, motWEB) VALUES ('" +
+      "SELECT '" +
       nom +
-      "', '" +
+      "','" +
       motWEB +
-      "')";
+      "' from profil where idProfil='" +
+      idProfil +
+      "'";
     connection.query(sql, function (err, result) {
       if (err) throw err;
       console.log('1 record inserted');
+      console.log(result);
     });
   });
 }
