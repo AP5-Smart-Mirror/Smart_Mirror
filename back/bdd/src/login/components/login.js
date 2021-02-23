b80e5d4b36ee131aef5e8cf6ea97fcede98dca42
Rainstorm <legrand.adrien96@gmail.com>
2021-02-23 16:58:04 +0100

    switch to knex

--- back/bdd/src/login/components/login.js
+++ back/bdd/src/login/components/login.js
@@ -1,26 +1,18 @@
-var connection = require('../../Connection/connection').mysql_pool;
+var db = require('../../Database/database');
 const bcrypt = require('bcryptjs');
 
-function getLogin(body) {
+async function getLogin(body) {
   var id = {};
   let name = body.username_account;
   let password = body.password_account;
-  connection.getConnection(function (err) {
-    if (err) throw err;
-    console.log('Connected!');
-    var sql =
-      "SELECT id_account,username_account,password_account from accounts where username_account='" +
-      name +
-      "'";
-    connection.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log(result[0]);
-      let isEqual = bcrypt.compareSync(password, result[0].password);
-      if (isEqual === true) {
-        id['id'] = result[0].idProfil;
-      }
+  var data = await db
+    .select('id', 'username')
+    .from('accounts')
+    .where('username', name)
+    .then((data) => {
+      return data;
     });
-  });
+  return data;
 }
 
 exports.getLogin = getLogin;
--- back/bdd/src/register/components/register.js
+++ back/bdd/src/register/components/register.js
@@ -1,25 +1,16 @@
-var connection = require('../../Connection/connection').mysql_pool;
+var db = require('../../Database/database');
 const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 
 async function getRegister(body) {
   const salt = bcrypt.genSaltSync(saltRounds);
-  const hash = bcrypt.hashSync(body.password_account, salt);
-  let name = body.username_account;
-  connection.getConnection(function (err, connection) {
-    if (err) throw err;
-    console.log('Connected!');
-    var sql =
-      "INSERT INTO accounts (username_account, password_account) VALUES ('" +
-      name +
-      "', '" +
-      hash +
-      "')";
-    connection.query(sql, function (err, result) {
-      if (err) throw err;
+  const hash = bcrypt.hashSync(body.password, salt);
+  let name = body.username;
+  await db('accounts')
+    .insert({ username: name, password: hash })
+    .then((data) => {
       console.log('1 record inserted');
     });
-  });
 }
 
 exports.getRegister = getRegister;
