12fb506b2727246bfc3370e88431d0bd9b2dc60c
Gaspard MATHON <gaspard.mathon@decathlon.com>
2021-02-23 09:33:26 +0100

    create account service

--- front/src/app/services/account.service.ts
+++ front/src/app/services/account.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { Widget } from '../models/widget';
+import { Account } from '../models/account';
 
 @Injectable({
   providedIn: 'root'
 })
-export class WidgetService {
+export class AccountService {
   private url: string = environment.serverBaseUrl;
 
   constructor(private httpClient: HttpClient) { }
 
-  getWidgets(idProfile: number): Promise<Widget[]> {
-    return new Promise<Widget[]>((resolve, reject) => {
-      this.httpClient.get<Widget[]>(this.url + '/widgets/' + idProfile)
+  getAccount(username: string): Promise<Account> {
+    return new Promise<Account>((resolve, reject) => {
+      this.httpClient.get<Account>(this.url + '/account/' + username)
         .toPromise()
         .then(
           res => { // Success
