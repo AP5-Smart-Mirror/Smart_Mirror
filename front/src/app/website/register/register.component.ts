09ae01a8e114ccb93a94366f10575dfd41e00d9f
Yoann Lefebvre <lefebvre.yoann@outlook.fr>
2021-02-23 11:12:36 +0100

    Partie graphique terminée

--- front/src/app/website/register/register.component.ts
+++ front/src/app/website/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators} from '@angular/forms';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,34 +10,20 @@ export class RegisterComponent implements OnInit {
 
   hide: boolean;
 
-  public frmSignup: FormGroup;
+	username = new FormControl(null, [
+		Validators.required,
+		Validators.minLength(3),
+	]);
 
-  constructor(private fb: FormBuilder) {
-    this.frmSignup = this.createSignupForm();
-  }
+  email = new FormControl(null, [
+    Validators.required,
+    Validators.email,
+  ]); 
 
-  createSignupForm(): FormGroup {
-    return this.fb.group(
-      {
-        email: [
-          null,
-          Validators.compose([Validators.email, Validators.required])
-        ],
-        password: [
-          null,
-          Validators.compose([
-            Validators.required,
-            Validators.minLength(8)
-          ])
-        ],
-        confirmPassword: [null, Validators.compose([Validators.required])]
-      },
-      {
-        // check whether our password and confirm password match
-        validator: this.passwordMatchValidator
-      }
-    );
-  }
+	password = new FormControl(null, [
+		Validators.required,
+		Validators.minLength(8),
+	]);
 
 	ngOnInit(): void {
 		this.hide = true;
@@ -46,16 +32,6 @@ export class RegisterComponent implements OnInit {
 	onSubmit(): void {
 		alert('You\'re registered');
 	}
-
-  passwordMatchValidator(control: AbstractControl) {
-    const password: string = control.get('password').value; // get password from our password form control
-    const confirmPassword: string = control.get('confirmPassword').value; // get password from our confirmPassword form control
-    // compare is the password math
-    if (password !== confirmPassword) {
-      // if they don't match, set an error in our confirmPassword form control
-      control.get('confirmPassword').setErrors({ NoPassswordMatch: true });
-    }
-  }
   
 }
 
