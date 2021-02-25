a2a4cc141ec58a04095c1a51ebea40b61b1941bb
Rainstorm <legrand.adrien96@gmail.com>
2021-02-25 12:09:50 +0100

    login account return profiles list

--- back/src/bdd/Account/components/login.js
+++ back/src/bdd/Account/components/login.js
@@ -2,17 +2,23 @@ var db = require('../../Database/database');
 const bcrypt = require('bcryptjs');
 
 async function getLogin(body) {
-  var id = {};
+  var result = {};
   let name = body.username;
   let password = body.password;
   var data = await db
     .select('id', 'username')
     .from('accounts')
-    .where('username', name)
-    .then((data) => {
-      return data;
-    });
-  return data;
+    .where('username', name);
+  result['id'] = data[0].id;
+
+  var listProfiles = await db
+    .select('id', 'username')
+    .from('profiles')
+    .where('id_account', data[0].id);
+
+  result['profiles'] = listProfiles;
+
+  return result;
 }
 
 exports.getLogin = getLogin;
