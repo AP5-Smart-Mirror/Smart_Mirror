97fc57fc07ed8950d0774561b6f2152042c17348
Rainstorm <legrand.adrien96@gmail.com>
2021-02-26 12:58:49 +0100

    add and delete profile widget

--- back/src/bdd/Widget/routes/widgetRouter.js
+++ back/src/bdd/Widget/routes/widgetRouter.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var addWidget = require('../components/addWidget');
 var deleteWidget = require('../components/deleteWidget');
+var addUserWidget = require('../components/addProfileWidget');
+var deleteUserWidget = require('../components/deleteProfileWidget');
 var router = express.Router();
 
 /* POST auth callback. */
@@ -8,9 +10,17 @@ router.post('/add', async function (req, res, next) {
   res.send(await addWidget.getAdd(req.body));
 });
 
+router.post('/addProfileWidget', async function (req, res, next) {
+  res.send(await addUserWidget.addUserWidget(req.body));
+});
+
 /* POST auth callback. */
 router.post('/delete', async function (req, res, next) {
   res.send(await deleteWidget.getDelete(req.body));
+});
+
+router.post('/deleteProfileWidget', async function (req, res, next) {
+  res.send(await deleteUserWidget.deleteUserWidget(req.body));
 });
 
 module.exports = router;
