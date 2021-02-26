209e96eba1b8c891dfe507b683fa530adf408711
Mike <sagemuanambelo@gmail.com>
2021-02-26 13:24:58 +0100

    pour cet aprem

---
+++ back/google_mail.js
@@ -0,0 +1,136 @@
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+
+// Load client secrets from a local file.
+fs.readFile('credentials.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Gmail API.
+  authorize(JSON.parse(content), listMail);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+/**
+ * Lists the labels in the user's account.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+
+function jsonTreatment(id,note,from,subject,date){
+    newJson = {};
+    newJson["date"] = date;
+    newJson["objet"] = subject;
+    newJson["expediteur"] = from;
+    newJson["id"] = id;
+    newJson["note"] = note;
+    return newJson;
+}
+const ids = [];
+var mesNotes = []
+function listMail(auth) {
+    const gmail = google.gmail({version: 'v1', auth});
+    gmail.users.messages.list({
+      userId: 'me',
+      labelIds: [
+        "INBOX",
+        "STARRED",
+      ],
+      "maxResults": 5
+    }, (err, res) => {
+      if (err) return console.log('The API returned an error: ' + err);
+      const messages = res.data.messages;
+      if (messages.length) {
+        console.log('Labels:');
+        messages.forEach((message) => {
+            ids.push(message.id);
+            //console.log(message.id);
+        });
+      } else {
+        console.log('No labels found.');
+      }
+      getMail(auth)
+    });
+  }
+function getMail(auth) {
+const gmail = google.gmail({version: 'v1', auth});
+  ids.forEach((id) => {
+    gmail.users.messages.get({
+        "userId": "me",
+        "id": id
+    },(err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const message = res.data.snippet;
+    let from = ""
+    let subject = ""
+    let date = ""
+    exps = res.data.payload.headers
+    exps.forEach((exp) =>{
+      if (exp.name == "From")
+      from = exp.value
+      if( exp.name == "Subject" )
+        subject = exp.value
+    if( exp.name == "Date" )
+        date = exp.value
+    })
+    console.log(jsonTreatment(id,message,from,subject,date))
+    })
+});
+}
+
+exports.getGmail = getMail
