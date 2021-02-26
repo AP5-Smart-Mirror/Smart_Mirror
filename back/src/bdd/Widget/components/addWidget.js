5f7dd1243d3acb2f22362fbeafe8816ab823dc89
Rainstorm <legrand.adrien96@gmail.com>
2021-02-26 12:04:07 +0100

    handle the knexjs return code

--- back/src/bdd/Widget/components/addWidget.js
+++ back/src/bdd/Widget/components/addWidget.js
@@ -1,14 +1,16 @@
 var db = require('../../Database/database');
 
 async function getAdd(body) {
+  var result = {};
   let widget = body.widget;
-  var data = await db
-    .insert({ widget: widget })
-    .from('widgets')
-    .then((data) => {
-      return data;
-    });
-  return data;
+  var res = await db.insert({ widget: widget }).from('widgets');
+  if (res != 0) {
+    result['id'] = res[0];
+    result['msg'] = 'Successfully created';
+  } else {
+    result['msg'] = 'Nothing was created';
+  }
+  return result;
 }
 
 exports.getAdd = getAdd;
--- back/src/bdd/Widget/components/deleteWidget.js
+++ back/src/bdd/Widget/components/deleteWidget.js
@@ -1,9 +1,15 @@
 var db = require('../../Database/database');
 
 async function getDelete(body) {
+  result = {};
   let id = body.id;
-  var data = await db('widgets').where({ id: id }).del();
-  return data;
+  var res = await db('widgets').where({ id: id }).del();
+  if (res != 0) {
+    result['msg'] = 'Successfully deleted';
+  } else {
+    result['msg'] = 'Nothing to delete';
+  }
+  return result;
 }
 
 exports.getDelete = getDelete;
