9cf7034747acd07403dd16eff1d27ac1cfeea6af
Rainstorm <legrand.adrien96@gmail.com>
2021-03-02 13:29:28 +0100

    switch the set widget to use widget name

--- back/src/bdd/Widget/components/setUserWidgets.js
+++ back/src/bdd/Widget/components/setUserWidgets.js
@@ -6,8 +6,12 @@ async function setUserWidgets(body) {
   var widgets = body.widgets;
   await db('associative').where({ id_profile: profile }).del();
   widgets.forEach(async (widget) => {
+    var data = await db
+      .select('id')
+      .from('widgets')
+      .where('widget', widget.widget);
     await db
-      .insert({ id_profile: profile, id_widget: widget.id })
+      .insert({ id_profile: profile, id_widget: data[0].id })
       .from('associative');
   });
   result['msg'] = 'done';
