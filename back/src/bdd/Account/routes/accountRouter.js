b1295511f96923c77bf99603c22e9a4c3cd0a97c
Rainstorm <legrand.adrien96@gmail.com>
2021-03-02 13:48:28 +0100

    get all first

--- back/src/bdd/Account/routes/accountRouter.js
+++ back/src/bdd/Account/routes/accountRouter.js
@@ -2,6 +2,7 @@ var express = require('express');
 var login = require('../components/login');
 var register = require('../components/register');
 var getAccount = require('../components/getAccount');
+var getAll = require('../components/getAll');
 var router = express.Router();
 
 /* POST auth callback. */
@@ -17,6 +18,11 @@ router.post('/register', async function (req, res, next) {
 /* POST auth callback. */
 router.post('/get_account', async function (req, res, next) {
   res.send(await getAccount.getAccount(req.body));
+});
+
+/* POST auth callback. */
+router.post('/get_all', async function (req, res, next) {
+  res.send(await getAll.getAll(req.body));
 });
 
 module.exports = router;
