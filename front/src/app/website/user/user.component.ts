6218282a64bf76bd9603b119dd625727e50d4f8c
Gaspard MATHON <gaspard.mathon@decathlon.com>
2021-03-02 23:20:52 +0100

    remove comments

--- front/src/app/website/user/user.component.ts
+++ front/src/app/website/user/user.component.ts
@@ -55,12 +55,6 @@ export class UserComponent implements OnInit {
 				});
 			})
 			.then(() => (this.loading = false));
-
-		/*this.profiles.push(new Profile(null, 'Gaspard', null, 'https://images.unsplash.com/photo-1463453091185-61582044d556?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'));
-    this.profiles.push(new Profile(null, 'Roger', null, 'https://images.unsplash.com/photo-1489702932289-406b7782113c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80'));
-    this.profiles.push(new Profile(null, 'Florian', null, 'https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80'));
-    this.profiles.push(new Profile(null, 'Manuelle', null, 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'));
-    this.profiles.push(new Profile(null, 'Invité', null, 'https://images.unsplash.com/photo-1614089119016-7a5ca1baa982?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80'));*/
 	}
 
 	choseProfile(id: number) {
