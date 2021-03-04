8fe2d2b9150439f12ccedc484f0dd45df4ed0438
Rainstorm <legrand.adrien96@gmail.com>
2021-03-04 09:20:38 +0100

    fix getAll to take the first line in db

--- back/src/bdd/Account/components/getAll.js
+++ back/src/bdd/Account/components/getAll.js
@@ -8,30 +8,29 @@ async function getAll(body) {
   var data = await db
     .select('id', 'username', 'password')
     .from('accounts')
-    .where('id', 1);
+    .limit(1);
 
-    result['id'] = data[0].id;
-    result['username'] = data[0].username;
-    var profiles = [];
-    var profilesDB = await db
-      .select('id', 'username')
-      .from('profiles')
-      .where('id_account',1);
-    console.log('SIZE', profilesDB.length);
-    for (var i = 0; i < profilesDB.length; i++) {
-      var newProfile = {};
-      newProfile['id'] = profilesDB[i].id;
-      newProfile['username'] = profilesDB[i].username;
-      newProfile['widgets'] = await db('profiles')
-        .join('associative', 'profiles.id', '=', 'associative.id_profile')
-        .join('widgets', 'widgets.id', '=', 'associative.id_widget')
-        .select('widgets.id', 'widgets.widget')
-        .where('associative.id_profile', profilesDB[i].id);
-      profiles.push(newProfile);
-    }
-    console.log(profiles);
-    result['profiles'] = profiles;
-
+  result['id'] = data[0].id;
+  result['username'] = data[0].username;
+  var profiles = [];
+  var profilesDB = await db
+    .select('id', 'username')
+    .from('profiles')
+    .where('id_account', 1);
+  console.log('SIZE', profilesDB.length);
+  for (var i = 0; i < profilesDB.length; i++) {
+    var newProfile = {};
+    newProfile['id'] = profilesDB[i].id;
+    newProfile['username'] = profilesDB[i].username;
+    newProfile['widgets'] = await db('profiles')
+      .join('associative', 'profiles.id', '=', 'associative.id_profile')
+      .join('widgets', 'widgets.id', '=', 'associative.id_widget')
+      .select('widgets.id', 'widgets.widget')
+      .where('associative.id_profile', profilesDB[i].id);
+    profiles.push(newProfile);
+  }
+  console.log(profiles);
+  result['profiles'] = profiles;
 
   return result;
 }
